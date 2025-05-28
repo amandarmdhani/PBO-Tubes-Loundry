b3b70e55fadb22f1272c77e6568536d14ba07594
khoerunnisasw <knisa8970@gmail.com>
2025-05-28 21:17:22 +0700

    Dashboard user

---
+++ src/main/java/com/example/crud/security/CustomLoginSuccessHandler.java
@@ -0,0 +1,27 @@
+package com.example.crud.security;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+public class CustomLoginSuccessHandler implements AuthenticationSuccessHandler {
+
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
+                                        Authentication authentication) throws IOException, ServletException {
+        boolean isAdmin = authentication.getAuthorities().contains(new SimpleGrantedAuthority("ADMIN"));
+
+        if (isAdmin) {
+            response.sendRedirect("/dashboardadmin");
+        } else {
+            response.sendRedirect("/dashboard");
+        }
+    }
+}
